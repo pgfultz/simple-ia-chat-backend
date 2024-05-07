adb14004454f49f61e27de53ac85c26c0e4fe0dc
pgfultz <lucas.s.morais23@gmail.com>
2024-05-07 11:23:05 -0300

    add mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 
-const app = express();
+mongoose.connect("mongodb://localhost:27017")
+  .then(() => {
+    console.log('Connected ');
 
-app.listen(process.env.PORT, () => {
-  console.log('Server is running on port: '+process.env.PORT)
-});
+    const app = express();
+
+    app.listen(process.env.PORT, () => {
+      console.log('Server is running on port: '+process.env.PORT)
+    });
+  })
+  .catch(err => console.log('MongoDb connect error'));
